7cdfe624f5d3c1520415af9bca632fb0a3a6fe1f
k-kahora <mango22mjk@gmail.com>
2023-12-12 03:29:42 +0000

    Eww widgets baby first one

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "web flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    # Do precise filtering of files in the nix store
+    nix-filter.url = "github:numtide/nix-filter";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, nix-filter}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = pkgs.lib;
+      in
+      {
+      devShells = {
+        default = pkgs.mkShell {
+          packages = [
+	    pkgs.eww-wayland
+          ];
+	  shellHook = ''
+	  export XDG_CONFIG_HOME="/home/malcolm/.config/"
+          '';
+        };
+
+       };
+     }
+    );
+}
