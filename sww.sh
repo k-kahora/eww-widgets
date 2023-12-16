77522e65207c33fdddc08a467c07df8feeb1d603
k-kahora <mango22mjk@gmail.com>
2023-12-16 05:33:06 +0000

    Finished state super happy with the result very sexy

--- sww.sh
+++ sww.sh
@@ -1,15 +1,15 @@
 directory="/home/malcolm/Documents/Wallpapers/"
-BUTTON_TOGGLE=$HOME/.button
-if [ ! -e "$BUTTON_TOGGLE" ]; then
-	touch "$BUTTON_TOGGLE"
-	eww update button-state="./button-clicked.png" --config ./
-else
-	rm "$BUTTON_TOGGLE"
-	eww update button-state="./button.png" --config ./
-fi
+# BUTTON_TOGGLE=$HOME/.button
+# if [ ! -e "$BUTTON_TOGGLE" ]; then
+# 	touch "$BUTTON_TOGGLE"
+# 	eww update button-state="./button-clicked.png" --config ./
+# else
+# 	rm "$BUTTON_TOGGLE"
+# 	eww update button-state="./button.png" --config ./
+# fi
 random_file=$(ls "$directory" | shuf -n 1)
 # (greeter :name "mal" :text "welcom")
 # (image :path "./button.png")
 swww init
-swww img $directory$random_file --transition-type grow --transition-pos 0.1,0.8 --transition-fps 200 
+swww img $directory$random_file --transition-type simple --transition-pos 0.95,0.9 --transition-fps 200 
 
