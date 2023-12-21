ebd8e750ea4f6ab9c4838d875d9a2f77e26e06a3
k-kahora <mango22mjk@gmail.com>
2023-12-21 02:52:42 +0000

    Build needed executer

--- builder.sh
+++ builder.sh
@@ -1,11 +1,21 @@
-#!/bin/sh
+# See all envar variables in derivation 
+# nix-store --read-log <derivation-path> to see the stdoutput of the build
+declare -xp
+# Run the below command in a nix repl with your loaded flake to take a look
+# It contains a bash script to set path variables for the builder
+# gives access to bash and gcc stuff as well as coreutils
+# builtins.toString outputs.packages.x86_64-linux.my-packges.stdenv
+source $stdenv/setup # bui
 
-# Ensure that the hello.sh script is executable
-# chmod +x $src/hello.sh
-cp $src/justfile $out/bin
-cp -r $src/Button $out/bin
-cp -r $src/eww.yuck $out/bin
-cp -r $src/eww.scss $out/bin
-cp -r $src/magic.sh $out/bin
-# Run the hello.sh script
 
+buildPhase() {
+    echo "... this is my custom build phase"
+    gcc foo.c -o foo
+}
+
+installPhase() {
+    mkdir -p $out/bin
+    cp foo $out/bin
+}
+
+genericBuild
--- flake.nix
+++ flake.nix
@@ -1,88 +1,40 @@
+
 {
-  description = "web flake";
+  description = "A very basic flake";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    nix-filter.url = "github:numtide/nix-filter";
+  outputs = { self, nixpkgs }: 
+  let 
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    inherit (nixpkgs) lib;
+  in
+  {
+    packages.x86_64-linux.mk-package = pkgs.stdenv.mkDerivation {
+      name = "wall-button";
+      # builder = ./builder.sh;
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+      src = ./.;
+      buildPhase = ''
+      echo "... this is my custom build phase"
+      echo "... nothing is done here"
+      '';
+      installPhase = ''
+      echo "... this is my custom install phase"
+      mkdir -p $out/bin
+      cp wall-button $out/bin
+      chmod +x $out/bin/wall-button
+      cp justfile $out/bin
+      cp -r Button $out/bin
+      cp -r art $out/bin
+      cp eww.scss $out/bin
+      cp eww.yuck $out/bin
+      cp magic.sh $out/bin
+        wrapProgram $out/bin/wall-button \
+        --suffix PATH : ${lib.makeBinPath [ pkgs.just pkgs.swww pkgs.eww ]}
+      '';
+    };
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.mk-package;
   };
-
-  outputs = { self, nixpkgs, flake-utils, nix-filter }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-	my-name = "test";
-	build = pkgs.stdenv.mkDerivation {
-	  src = ./.;
-	  name = "wow";
-	  buildInputs = [pkgs.coreutils];
-	  buildPhase = ''
-	  mkdir -p $out/bin
-	  out_saved=$out
-	  echo "eww open example --config $out_saved/bin"  >> runner.sh
-	  echo "echo failure"  >> runner.sh
-	  '';
-	  
-	  installPhase = ''
-	  cp $src/justfile $out/bin
-	  cp $src/eww.scss $out/bin
-	  cp -r $src/art $out/bin
-	  cp $src/eww.yuck $out/bin
-	  # cp $src/runner.sh $out/bin
-	  cp -r $src/Button $out/bin
-	  cp $src/magic.sh $out/bin
-	  cp runner.sh $out/bin
- 	  '';
-
-        };
-        my-script = (pkgs.writeScriptBin my-name (builtins.readFile ./runner.sh)).overrideAttrs(old: {
-
-          buildCommand = "${old.buildCommand}\n patchShebangs $out";
-
-        });
-      in
-      {
-        packages = {
-	  
-          my-script = 
-          let 
-	    wow = "wow";
-
-	    scriptor = pkgs.writeShellScriptBin my-name ''
-	    out_saved=$out
-	    echo $out_saved
-	    echo $out
-	    eww open example --config $out_saved/bin
-
-	    '';
-	    in pkgs.symlinkJoin {
-	    src = self;
-	    name = my-name;
-	    paths = [build] ++ [scriptor] ++ [pkgs.swww pkgs.just pkgs.eww-wayland];
-	    buildInputs = [pkgs.makeWrapper];
-	    # TODO Build phase is not doing anything 
-	    # The directory is not gettin created
-	    # result does not contain these files
-            postBuild = ''
- 	    wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin
-	    echo 
-	    '';
-};
-        };
-
-        defaultPackage = self.packages.${system}.my-script;
-
-        devShell = pkgs.mkShell {
-          packages = [
-            pkgs.eww-wayland
-            pkgs.just
-
-            pkgs.swww
-          ];
-          shellHook = ''
-            export XDG_CONFIG_HOME="/home/malcolm/.config/"
-          '';
-        };
-      }
-    );
 }
